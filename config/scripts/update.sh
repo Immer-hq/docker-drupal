54423163e764521762d3986994fdf058d004579b
Maurits Lawende <mauritsl@users.noreply.github.com>
2026-02-24 13:12:51 +0100

    Use drush deploy in deploy script

--- config/scripts/update.sh
+++ config/scripts/update.sh
@@ -10,18 +10,10 @@ fi
 
 for x in `ls /var/www/web/sites`; do
   if [ -f "/var/www/web/sites/$x/settings.php" ]; then
-    drush -l $x cr
-    drush -l $x -y updb
-    if [ -n "$SKIP_WEBFORM_IMPORT" ]; then
-      mkdir -p /tmp/config-export/
-      drush config-export -l $x -y --destination=/tmp/config-export/
-      cp /tmp/config-export/webform.webform* /var/www/config/sync/
-      rm -Rf /tmp/config-export
-    fi
-    drush -l $x -y cim
-    drush -l $x cr
+    drush -l $x deploy
     if [ -f "../translations/nl.po" ]; then
       drush locale:import nl ../translations/nl.po | cat
+      drush -l $x cr
     fi
   fi
 done
